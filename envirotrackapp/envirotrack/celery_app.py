c85ac5c69d8d3e887833bd294d5b329ae1245231
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-10-03 20:24:30 +0500

    some fixes with configurate celery

--- envirotrackapp/envirotrack/celery_app.py
+++ envirotrackapp/envirotrack/celery_app.py
@@ -4,11 +4,11 @@ import time
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'envirotrack.settings')
 
 app = Celery('envirotrack')
 app.config_from_object('django.conf:settings')
-app.conf.broker_url = settings.CELERY_BROCKER_URL
+app.conf.broker_url = settings.CELERY_BROKER_URL
 app.autodiscover_tasks()
 
 @app.task()
