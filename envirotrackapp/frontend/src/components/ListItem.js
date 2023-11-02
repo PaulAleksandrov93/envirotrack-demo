fc62afe86e5edcd526282240e1782a34dfc8ee88
PaulAleksandrov93 <aleksandrov.pavel93@gmail.com>
2023-11-02 20:21:09 +0500

    Changed the structure of the entry display in the list of entries

--- envirotrackapp/frontend/src/components/ListItem.js
+++ envirotrackapp/frontend/src/components/ListItem.js
@@ -1,6 +1,24 @@
+// ListItem.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './ListItem.css';
+
+const renderParameterSets = (parameterSets) => {
+  if (!parameterSets || parameterSets.length === 0) return '-';
+
+  return parameterSets.map((paramSet, index) => (
+    <div key={index} className="parameter-set">
+      <div className="parameter-item">
+        <span>Набор {index + 1}:</span>
+        <span> Температура: {paramSet.temperature_celsius},</span>
+        <span> Влажность: {paramSet.humidity_percentage},</span>
+        <span> Давление (кПа): {paramSet.pressure_kpa},</span>
+        <span> Давление (мм рт. ст.): {paramSet.pressure_mmhg},</span>
+        <span> Время создания: {paramSet.time}</span>
+      </div>
+    </div>
+  ));
+};
 
 const ListItem = ({ parameter }) => {
   const getTime = (dateString) => {
@@ -8,31 +26,21 @@ const ListItem = ({ parameter }) => {
     return date.toLocaleDateString();
   };
 
-  const renderParameterSets = (parameterSets) => {
-    if (!parameterSets || parameterSets.length === 0) return '-';
-
-    return parameterSets.map((paramSet, index) => (
-      <div key={index} className="parameter-set">
-        <div>Температура (°C): {paramSet.temperature_celsius}</div>
-        <div>Влажность (%): {paramSet.humidity_percentage}</div>
-        <div>Давление (кПа): {paramSet.pressure_kpa}</div>
-        <div>Давление (мм рт. ст.): {paramSet.pressure_mmhg}</div>
-        <div>Время создания: {paramSet.time}</div>
-      </div>
-    ));
-  };
-
   return (
     <Link to={`/parameter/${parameter.id}`}>
       <div className="parameters-list-item">
         <h3>Помещение: {parameter.room.room_number} | Ответственный: {parameter.responsible.first_name} {parameter.responsible.last_name}</h3>
-        <div className="parameters">
-          {renderParameterSets(parameter.parameter_sets)}
-          <div className="parameter-item">
-            <span>Дата и время:</span> {getTime(parameter.created_at)}
+        <div className="parameters-and-info">
+          <div className="parameters">
+            {renderParameterSets(parameter.parameter_sets)}
           </div>
-          <div className="parameter-item">
-            <span>Средство измерения:</span> {parameter.measurement_instrument ? parameter.measurement_instrument.name : 'Нет информации'}
+          <div className="info">
+            <div className="parameter-item">
+              <span>Дата и время:</span> {getTime(parameter.created_at)}
+            </div>
+            <div className="parameter-item">
+              <span>Средство измерения:</span> {parameter.measurement_instrument ? parameter.measurement_instrument.name : 'Нет информации'}
+            </div>
           </div>
         </div>
       </div>
